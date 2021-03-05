0aa98166f6208a67435ba173af721e1c754488da
Kestone Dennis <55648876+kes-crypto@users.noreply.github.com>
2021-03-05 20:15:29 +0300

    birthday surprise code for your friends

---
+++ scripts/index.js
@@ -0,0 +1,388 @@
+const count = document.getElementById('count');
+const head = document.getElementById('head');
+const giftbox = document.getElementById('merrywrap');
+const canvasC = document.getElementById('c');
+const content = document.getElementById('container');
+
+const config = {
+  birthdate: 'June 25, 2021',
+  name: 'Verah'
+};
+
+function hideEverything() {
+  head.style.display = 'none';
+  count.style.display = 'none';
+  giftbox.style.display = 'none';
+  canvasC.style.display = 'none';
+  //content.style.display = 'none';
+}
+
+hideEverything();
+
+const confettiSettings = { target: 'confetti' };
+const confetti = new window.ConfettiGenerator(confettiSettings);
+confetti.render();
+
+const second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+
+let countDown = new Date(`${config.birthdate} 00:00:00`).getTime();
+x = setInterval(function() {
+  let now = new Date().getTime(),
+    distance = countDown - now;
+
+  document.getElementById('day').innerText = Math.floor(distance / day);
+  document.getElementById('hour').innerText = Math.floor(
+    (distance % day) / hour
+  );
+  document.getElementById('minute').innerText = Math.floor(
+    (distance % hour) / minute
+  );
+  document.getElementById('second').innerText = Math.floor(
+    (distance % minute) / second
+  );
+
+
+  window.addEventListener('resize', function() {
+    w = c.width = window.innerWidth;
+    h = c.height = window.innerHeight;
+
+    hw = w / 2;
+    hh = h / 2;
+
+    ctx.font = opts.charSize + 'px Verdana';
+  });
+
+  if (distance > 0) {
+    head.style.display = 'initial';
+    count.style.display = 'initial';
+  } else {
+    head.style.display = 'none';
+    count.style.display = 'none';
+    giftbox.style.display = 'initial';
+    clearInterval(x);
+    let merrywrap = document.getElementById('merrywrap');
+    let box = merrywrap.getElementsByClassName('giftbox')[0];
+    let step = 1;
+    let stepMinutes = [200, 200, 100, 100];
+
+    function init() {
+      box.addEventListener('click', openBox);
+      box.addEventListener('click', showfireworks, false);
+    }
+
+    function stepClass(step) {
+      merrywrap.className = 'merrywrap';
+      merrywrap.className = 'merrywrap step-' + step;
+    }
+
+    function openBox() {
+      if (step === 1) {
+        box.removeEventListener('click', openBox);
+      }
+      stepClass(step);
+      if (step === 3) {
+      }
+      if (step === 4) {
+        return;
+      }
+      setTimeout(openBox, stepMinutes[step - 1]);
+      step++;
+       
+    }
+
+    function showfireworks() {
+      // Animation Timeline
+const animationTimeline = () => {
+  // Spit chars that needs to be animated individually
+  const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
+  const hbd = document.getElementsByClassName("wish-hbd")[0];
+
+  textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
+    .split("")
+    .join("</span><span>")}</span`;
+
+  hbd.innerHTML = `<span>${hbd.innerHTML
+    .split("")
+    .join("</span><span>")}</span`;
+
+  const ideaTextTrans = {
+    opacity: 0,
+    y: -20,
+    rotationX: 5,
+    skewX: "15deg"
+  };
+
+  const ideaTextTransLeave = {
+    opacity: 0,
+    y: 20,
+    rotationY: 5,
+    skewX: "-15deg"
+  };
+
+  const tl = new TimelineMax();
+
+  tl
+    .to(".container", 0.1, {
+      visibility: "visible"
+    })
+    .from(".one", 0.7, {
+      opacity: 0,
+      y: 10
+    })
+    .from(".two", 0.4, {
+      opacity: 0,
+      y: 10
+    })
+    .to(
+      ".one",
+      0.7,
+      {
+        opacity: 0,
+        y: 10
+      },
+      "+=2.5"
+    )
+    .to(
+      ".two",
+      0.7,
+      {
+        opacity: 0,
+        y: 10
+      },
+      "-=1"
+    )
+    .from(".three", 0.7, {
+      opacity: 0,
+      y: 10
+      // scale: 0.7
+    })
+    .to(
+      ".three",
+      0.7,
+      {
+        opacity: 0,
+        y: 10
+      },
+      "+=2"
+    )
+    .from(".four", 0.7, {
+      scale: 0.2,
+      opacity: 0
+    })
+    .from(".fake-btn", 0.3, {
+      scale: 0.2,
+      opacity: 0
+    })
+    .staggerTo(
+      ".hbd-chatbox span",
+      0.5,
+      {
+        visibility: "visible"
+      },
+      0.05
+    )
+    .to(".fake-btn", 0.1, {
+      backgroundColor: "rgb(127, 206, 248)"
+    })
+    .to(
+      ".four",
+      0.5,
+      {
+        scale: 0.2,
+        opacity: 0,
+        y: -150
+      },
+      "+=0.7"
+    )
+    .from(".idea-1", 0.7, ideaTextTrans)
+    .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-2", 0.7, ideaTextTrans)
+    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-3", 0.7, ideaTextTrans)
+    .to(".idea-3 strong", 0.5, {
+      scale: 1.2,
+      x: 10,
+      backgroundColor: "rgb(21, 161, 237)",
+      color: "#fff"
+    })
+    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(".idea-4", 0.7, ideaTextTrans)
+    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
+    .from(
+      ".idea-5",
+      0.7,
+      {
+        rotationX: 15,
+        rotationZ: -10,
+        skewY: "-5deg",
+        y: 50,
+        z: 10,
+        opacity: 0
+      },
+      "+=0.5"
+    )
+    .to(
+      ".idea-5 span",
+      0.7,
+      {
+        rotation: 90,
+        x: 8
+      },
+      "+=0.4"
+    )
+    .to(
+      ".idea-5",
+      0.7,
+      {
+        scale: 0.2,
+        opacity: 0
+      },
+      "+=2"
+    )
+    .staggerFrom(
+      ".idea-6 span",
+      0.8,
+      {
+        scale: 3,
+        opacity: 0,
+        rotation: 15,
+        ease: Expo.easeOut
+      },
+      0.2
+    )
+    .staggerTo(
+      ".idea-6 span",
+      0.8,
+      {
+        scale: 3,
+        opacity: 0,
+        rotation: -15,
+        ease: Expo.easeOut
+      },
+      0.2,
+      "+=1"
+    )
+    .staggerFromTo(
+      ".baloons img",
+      2.5,
+      {
+        opacity: 0.9,
+        y: 1400
+      },
+      {
+        opacity: 1,
+        y: -1000
+      },
+      0.2
+    )
+    .from(
+      ".stanley-dp",
+      0.5,
+      {
+        scale: 3.5,
+        opacity: 0,
+        x: 25,
+        y: -25,
+        rotationZ: -45
+      },
+      "-=2"
+    )
+    .from(".hat", 0.5, {
+      x: -100,
+      y: 350,
+      rotation: -180,
+      opacity: 0
+    })
+    .staggerFrom(
+      ".wish-hbd span",
+      0.7,
+      {
+        opacity: 0,
+        y: -50,
+        // scale: 0.3,
+        rotation: 150,
+        skewX: "30deg",
+        ease: Elastic.easeOut.config(1, 0.5)
+      },
+      0.1
+    )
+    .staggerFromTo(
+      ".wish-hbd span",
+      0.7,
+      {
+        scale: 1.4,
+        rotationY: 150
+      },
+      {
+        scale: 1,
+        rotationY: 0,
+        color: "#ff69b4",
+        ease: Expo.easeOut
+      },
+      0.1,
+      "party"
+    )
+    .from(
+      ".wish h5",
+      0.5,
+      {
+        opacity: 0,
+        y: 10,
+        skewX: "-15deg"
+      },
+      "party"
+    )
+    .staggerTo(
+      ".eight svg",
+      1.5,
+      {
+        visibility: "visible",
+        opacity: 0,
+        scale: 80,
+        repeat: 3,
+        repeatDelay: 1.4
+      },
+      0.3
+    )
+    .to(".six", 0.5, {
+      opacity: 0,
+      y: 30,
+      zIndex: "-1"
+    })
+    .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
+    .to(
+      ".last-smile",
+      0.5,
+      {
+        rotation: 90
+      },
+      "+=1"
+    );
+
+  // tl.seek("currentStep");
+  // tl.timeScale(2);
+
+  // Restart Animation on click
+  const replyBtn = document.getElementById("replay");
+  replyBtn.addEventListener("click", () => {
+    tl.restart();
+  });
+};
+
+animationTimeline()
+
+    }
+    
+
+    init();
+    
+  }
+
+  // if (distance < 0) {
+  //     clearInterval(x);
+  //     console.log("happy birthday");
+  // }
+}, second);
