d2574dc12aad9ae60ad295101b7608f5a50583dc
Kestone Dennis <denniskestone@gmail.com>
2023-07-28 09:17:24 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const canvasC = document.getElementById('c');
 const content = document.getElementById('container');
 
 const config = {
-  birthdate: 'June 25, 2021',
+  birthdate: 'June 25, 2024',
   name: 'Verah'
 };
 
